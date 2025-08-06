bab43cf8c007a8d5976e985ab7e4e943e55e2ef5
colecharb <40375497+colecharb@users.noreply.github.com>
2025-08-06 16:29:07 -0400

    feat(sketchbook): adds sketch component with video embeds

---
+++ src/components/SketchbookEntry.tsx
@@ -0,0 +1,21 @@
+import type { PropsWithChildren } from 'react';
+
+export type SketchProps = PropsWithChildren<{
+  title: string;
+  abstract: string;
+}>;
+
+function Sketch(props: SketchProps & { className?: string }) {
+  const { title, abstract, children, className } = props;
+  return (
+    <div className={`flex py-10 gap-10 ${className}`}>
+      <div className=''>
+        <h3 className='mt-0'>{title}</h3>
+        {abstract}
+      </div>
+      {children}
+    </div>
+  );
+}
+
+export default Sketch;
--- src/routes/Sketchbook.tsx
+++ src/routes/Sketchbook.tsx
@@ -1,35 +1,71 @@
+import Sketch, { type SketchProps } from '../components/SketchbookEntry';
+
+const SKETCHES: SketchProps[] = [
+  {
+    title: 'Animation Projects',
+    abstract:
+      'A collection of animation projects exploring various techniques and storytelling methods.',
+  },
+  {
+    title: 'R+R Video',
+    abstract:
+      'A video project available on YouTube. Watch it here: https://www.youtube.com/watch?v=ZYlsVHf5XOY',
+    children: (
+      <iframe
+        src='https://www.youtube.com/embed/ZYlsVHf5XOY'
+        title='R+R Video'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        allowFullScreen
+        style={{ marginTop: '1rem', maxWidth: '100%' }}
+      />
+    ),
+  },
+  {
+    title: 'Ironself Movie Trailer',
+    abstract:
+      "Trailer for the movie 'Ironself'. Watch it here: https://www.youtube.com/watch?v=twhPZk0amV0",
+    children: (
+      <iframe
+        src='https://www.youtube.com/embed/twhPZk0amV0'
+        title='Ironself Movie Trailer'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        allowFullScreen
+        style={{ marginTop: '1rem', maxWidth: '100%' }}
+      />
+    ),
+  },
+  {
+    title: 'Seattle Black Panther Doc Trailer',
+    abstract:
+      'Trailer for the Seattle Black Panther documentary. Watch it here: https://youtu.be/yVm8j-ZMfYU',
+    children: (
+      <iframe
+        src='https://www.youtube.com/embed/yVm8j-ZMfYU'
+        title='Seattle Black Panther Doc Trailer'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        allowFullScreen
+        style={{ marginTop: '1rem', maxWidth: '100%' }}
+      />
+    ),
+  },
+  {
+    title: 'Film Photography',
+    abstract:
+      'A series of film photography works capturing unique moments and perspectives.',
+  },
+];
+
 const Sketchbook = () => {
   return (
     <>
       <h2>Sketch Book</h2>
-      <ul>
-        <li>[animation projects]</li>
-        <li>
-          <a
-            target='_blank'
-            href='https://www.youtube.com/watch?v=ZYlsVHf5XOY'
-          >
-            R+R Video
-          </a>
-        </li>
-        <li>
-          <a
-            target='_blank'
-            href='https://www.youtube.com/watch?v=twhPZk0amV0'
-          >
-            Ironself Movie Trailer
-          </a>
-        </li>
-        <li>
-          <a
-            target='_blank'
-            href='https://youtu.be/yVm8j-ZMfYU'
-          >
-            Seattle Black Panther Doc Trailer
-          </a>
-        </li>
-        <li>film photography</li>
-      </ul>
+      {SKETCHES.map((sketch, index) => (
+        <Sketch
+          className={index % 2 === 0 ? 'flex-row' : 'flex-row-reverse'}
+          key={sketch.title}
+          {...sketch}
+        />
+      ))}
     </>
   );
 };

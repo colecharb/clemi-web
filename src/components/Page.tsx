da6a5cac1667c3220933ed3d60d1fbc12ec7b35e
colecharb <40375497+colecharb@users.noreply.github.com>
2025-08-23 17:02:58 -0400

    fix: vercel image fix

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -12,7 +12,7 @@ const Page = ({ children }: PropsWithChildren) => {
       {children}
       <div className='z-10 fixed top-0 left-0 right-0 h-30 bg-gradient-to-b from-[var(--background)] to-transparent' />
       <img
-        src='/src/assets/images/campfire.gif'
+        src='/images/campfire.gif'
         className='z-20 fixed bottom-[90px] sm:bottom-5 right-0 drop-shadow-[var(--background)] w-[40vw] max-w-[250px]'
       />
       <div className='z-10 fixed bottom-0 left-0 right-0 h-[30vh] bg-gradient-to-t from-black to-transparent ' />

c4b8a2878049c3dc063de4f26299cf34be58f1e6
colecharb <40375497+colecharb@users.noreply.github.com>
2025-09-04 11:42:40 -0400

    chore: prepares photos

---
+++ src/photos.ts
@@ -0,0 +1,22 @@
+const photos = [
+  "/images/Photos/BW-Mate.JPG",
+  "/images/Photos/BWReflections.JPG",
+  "/images/Photos/Balloons.JPG",
+  "/images/Photos/BelowtheSurface.JPG",
+  "/images/Photos/CasaAbuelos.JPG",
+  "/images/Photos/ColeCigar.JPG",
+  "/images/Photos/CoralPinkDunes.JPG",
+  "/images/Photos/Horizon.JPG",
+  "/images/Photos/LenoxLakes.JPG",
+  "/images/Photos/LetSleepingDogsLie.JPG",
+  "/images/Photos/MercadoArgentino.JPG",
+  "/images/Photos/Pharoah.JPG",
+  "/images/Photos/PlayaSunset.JPG",
+  "/images/Photos/PrimaCool.JPG",
+  "/images/Photos/River.JPG",
+  "/images/Photos/SikuHorse.JPG",
+  "/images/Photos/Uruguay.JPG",
+  "/images/Photos/UtahZoom.JPG",
+  "/images/Photos/Watchman.JPG"
+];
+export default photos;
